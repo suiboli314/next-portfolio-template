9c1e4549a43e59538a0f5966b9750c8cc5b4e1fb
suiboli314 <23085379+suiboli314@users.noreply.github.com>
2023-09-03 02:58:26 -0700

    delete all components test recursion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,9 @@
-import { Box } from "@mui/material";
-
-import ThemeItem from "@/components/themeItem";
-import VerticalSidebar from "@/components/Navbar/verticalSidebar";
-
 import "./page.css";
-import Intro from "@/components/Introduction";
 
 export const GLOBAL_WIDTH = "calc(100% - 64px)";
 
 export default function Home() {
   return (
-    <ThemeItem>
-      <VerticalSidebar>{/* <></> */}</VerticalSidebar>
-      <Box
-        sx={{
-          width: {
-            xs: "100%",
-            md: GLOBAL_WIDTH,
-          }
-        }}
-      >
-        <main>
-          {/* <Intro/> */}
-          
-        </main>
-      </Box>
-      
-    </ThemeItem>
+    <></>
   );
 }
--- src/components/Carousel/imageByIndex.ts
+++
@@ -1,8 +0,0 @@
-import image1 from "@/images/bookratings.png";
-import image2 from "@/images/avatar.jpg";
-
-export const images: string[] = [image1.src, image2.src];
-
-const imageByIndex = (index: number): string => images[index % images.length];
-
-export default imageByIndex;
